9ff286fab6863fb64fc04a19be1c54d0ad2f6670
Andrew McGaffney <andrewm@thedigitaledge.com.au>
2019-08-15 14:30:55 +1000

    Updated javascript to log click events.

--- js/main.js
+++ js/main.js
@@ -17,5 +17,25 @@ img03.addEventListener('click', function () {
   modalImg.src='img/3-Card_CTA/Card-03/Image-03.jpg';
 });
 
+//capture all anchor clicks
+function respondToTheClick (event) {
+  event.preventDefault();
+  
+  if (event.target.id==='logclick1' || event.target.id==='logclick2' || event.target.id==='logclick3') {      
+      event.target.parentElement.parentElement.previousElementSibling.textContent = 'Link disabled to show event capture in console';
+      event.target.parentElement.parentElement.previousElementSibling.style.cssText = 'display:block;';
+      setTimeout(function(){
+        event.target.parentElement.parentElement.previousElementSibling.style.cssText = 'display:none;';
+      }, 2000);   
+  }
+    console.log('Element clicked was: ' + event.target.id)
+}
+
+const allClicks = document.querySelectorAll('a');
+
+for(let i = 0; i < allClicks.length; i++){     
+    allClicks[i].addEventListener('click', respondToTheClick);      
+}
+
 
 
